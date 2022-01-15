d8717e27c7cb9e8e15d18d83468d9e7344309146
King <1664233781@qq.com>
2022-01-15 19:27:35 +0800

    ex

--- 练习/scripts/main.js
+++ 练习/scripts/main.js
@@ -1,2 +1,4 @@
-let myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello World!';
+baz(); //Uncaught TypeError: baz is not a function/类型错误：baz 不是一个函数
+var baz = function() {
+  console.log("bar2");
+};
